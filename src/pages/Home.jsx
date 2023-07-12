2289da59133c38657ce93c5bd10f39a753079a8a
UHquokka <bananamayong@gmail.com>
2023-07-12 17:03:10 +0900

    search page 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import Header from '../components/Header/Header';
 import Videos from '../components/Videos/Videos';
 
 export default function Home() {
---
+++ src/pages/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import SearchVideos from '../components/Search/SearchVideos';
+
+export default function Search() {
+    return (
+        <div>
+            <SearchVideos />
+        </div>
+    );
+}
+
