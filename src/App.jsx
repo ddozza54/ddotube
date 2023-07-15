ae1aece1fa4592f6c946c47bbe17f38415105bff
UHquokka <bananamayong@gmail.com>
2023-07-15 18:09:10 +0900

    router 구조 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,22 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
-import NotFound from './pages/NotFound';
-import Root from './pages/Root';
-import Home from './pages/Home';
-import VideoPlayer from './pages/VideoPlayer';
-import Videos from './components/Videos';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: `videos`, element: <Home /> },
-      { path: `videos/:keyword`, element: <Videos /> },
-      { path: `video/watch/:id`, element: <VideoPlayer /> },
-      // { path: `/video/:id([0-9a-fA_Z-_]{11})`, element: <videos /> }
-    ]
-  }
-])
+import Header from './components/Header';
 
 const queryClient = new QueryClient();
 
+//쿼리는 가장 근접한 부모에 해줘야함. header 는 
 function App() {
-  return (
+  return (<>
+    <Header />
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <Outlet />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  );
+  </>);
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import NotFound from './pages/NotFound';
+import VideoPlayer from './pages/VideoPlayer';
+import Videos from './components/Videos';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Videos /> },
+      { path: `videos`, element: <Videos /> },
+      { path: `videos/:keyword`, element: <Videos /> },
+      { path: `video/watch/:id`, element: <VideoPlayer /> },
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
